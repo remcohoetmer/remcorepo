c5aa881d3a514ea547571eb7c07ce4923a35906e
REMCO\Remco <remcohoetmer@gmail.com>
2016-09-16 16:03:53 +0200

    add file plugin

--- src/nl/remco/remcoproject/Main.java
+++ src/nl/remco/remcoproject/Main.java
@@ -3,6 +3,8 @@ package nl.remco.remcoproject;
 public class Main {
 	public static void main (String[] args)
 	{
+		FilePlugin fp= new FilePlugin();
+		
 		String encoded= new Sha().sha256( "secret");
 		System.out.println("Encoded:"+ encoded);
 		//return 0;
