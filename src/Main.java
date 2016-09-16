39e4f512781f59799903385eeb4d1102ab83d841
REMCO\Remco <remcohoetmer@gmail.com>
2016-09-16 15:19:57 +0200

    added main

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.security.MessageDigest;
+
+public class Main {
+	public static void main (String[] args)
+	{
+		String encoded= sha256( "secret");
+		System.out.println("Encoded:"+ encoded);
+		//return 0;
+	}
+	public static String sha256(String base) {
+		try{
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] hash = digest.digest(base.getBytes("UTF-8"));
+			StringBuffer hexString = new StringBuffer();
+
+			for (int i = 0; i < hash.length; i++) {
+				String hex = Integer.toHexString(0xff & hash[i]);
+				if(hex.length() == 1) hexString.append('0');
+				hexString.append(hex);
+			}
+
+			return hexString.toString();
+		} catch(Exception ex){
+			throw new RuntimeException(ex);
+		}
+	}
+}
