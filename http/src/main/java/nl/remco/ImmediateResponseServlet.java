05dafaaf2493fc3b94d3286e853f9f6549fc713e
REMCO\Remco <remcohoetmer@gmail.com>
2017-01-24 20:29:14 +0100

    .

--- http/src/main/java/nl/remco/ImmediateResponseServlet.java
+++ http/src/main/java/nl/remco/ImmediateResponseServlet.java
@@ -9,10 +9,11 @@ import java.io.OutputStreamWriter;
 import java.io.Reader;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+@WebServlet("/im")
 public class ImmediateResponseServlet extends HttpServlet{
 	private static final long serialVersionUID = 1;
 
